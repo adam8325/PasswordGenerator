f8adab8fd3d7792df429682d8ac68acb5c2daa02
Adam Eleraky <adam_eleraky@hotmail.com>
2024-07-29 10:12:33 +0200

    Final css changes made

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+
+let password1 = document.getElementById("password1");
+let password2 = document.getElementById("password2");
+
+function generateCode() {
+    let password1Str = '';
+    let password2Str = '';
+
+    for (let i = 0; i < 15; i++) {
+        let randomIndex1 = Math.floor(Math.random() * characters.length);
+        let randomIndex2 = Math.floor(Math.random() * characters.length);
+        password1Str += characters[randomIndex1];
+        password2Str += characters[randomIndex2];
+    }
+
+    password1.textContent = password1Str;
+    password2.textContent = password2Str;
+}
